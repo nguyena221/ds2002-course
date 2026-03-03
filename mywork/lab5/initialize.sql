195033665c9a662f88fb78445dd1bb1232980b4f
Annie Nguyen <nguyenannie221@gmail.com>
2026-03-02 19:09:01 -0500

    merge

--- mywork/lab5/initialize.sql
+++ mywork/lab5/initialize.sql
@@ -1,3 +1,7 @@
+USE bvc5vq_db; 
+DROP TABLE IF EXISTS user_posts;
+DROP TABLE IF EXISTS user_info;
+
 CREATE TABLE user_info (
 	user_id INT AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(50),
@@ -13,7 +17,29 @@ CREATE TABLE user_posts (
     FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
-USE user_info; 
-INSERT INTO users (first_name, last_name, email) VALUES ('Annie', 'Nguyen', 'bvc5vq@virginia.edu');
 
-SELECT * FROM users;
+INSERT INTO user_info (first_name, last_name, email) 
+VALUES 
+('Annie', 'Nguyen', 'bvc5vq@virginia.edu'), 
+('Emma', 'Chung', 'hng0pv@virginia.edu'),
+('Andrew', 'Nguyen', 'fru4yr@virginia.edu'),
+('Emujin', 'Batzorig', 'yur9pw@virginia.edu'),
+('Jia', 'Park', 'uy23mn@virginia.edu'), 
+('Aiden', 'Nguyen', 'tyr6up@virginia.edu'),
+('Teri', 'Phan', 'bhn9er@virginia.edu'),
+('Andrew', 'Nguyen', 'tye0jk@virginia.edu'),
+('Stephanie', 'Lim', 'sth4nr@virginia.edu'),
+('Thea', 'Budlong', 'uol7qg@virginia.edu');
+
+INSERT INTO user_posts (user_id, post_text, date_posted)
+VALUES
+(1, 'Excited to join the platform!', '2026-02-23 09:00:00'),
+(2, 'Hello everyone!', '2026-02-23 09:15:00'),
+(3, 'Working on my SQL lab today.', '2026-02-23 09:30:00'),
+(4, 'Just finished my first post.', '2026-02-23 10:00:00'),
+(5, 'Learning about foreign keys.', '2026-02-23 10:20:00'),
+(6, 'Auto increment makes life easier.', '2026-02-23 10:45:00'),
+(7, 'Database relationships are cool.', '2026-02-23 11:00:00'),
+(8, 'Practicing SQL joins next.', '2026-02-23 11:30:00'),
+(9, 'Almost done with this lab!', '2026-02-23 12:00:00'),
+(10, 'Time for lunch after coding.', '2026-02-23 12:30:00');
